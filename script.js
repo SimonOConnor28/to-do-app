d18fcd4a3a8e346cf24da5dec4868e9be7812073
SimonOConnor28 <simonoconnor28@gmail.com>
2022-05-11 14:40:35 -0400

    add JS file and pseudocode

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// add a submit even listen on the form
+
+    // stop the page from refreshing when form is submitted
+
+    // only if the user has entered an actual task (AKA input is not empty)
+
+    // Grab the user's to d item from the form input
+    // display TO DO on the page within an li element
+        // include a checkbox icon within the li
+    // add the li element to do ul
+
+
+// clicking on a task allows you to toogle between checked/unchecked (aka done vs not done)
+
+
+
+// BONUS LEVEL 
+// add a "reset" button which clears all of the TO DOs
+// add a "remove task" button to each task
+// add a congratulations alert when all of the existing TO DOs are checked off
+// add a "Take a break" message if 5 or more task are completed 
+
+
